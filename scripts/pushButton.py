84291e182da27602c3c37302be182306aac6d659
Joel <joel.hamilton@fusionnetworks.co.nz>
2021-05-12 11:02:05 +1200

    try some stuff

--- scripts/pushButton.py
+++ scripts/pushButton.py
@@ -1,18 +1,21 @@
 import RPi.GPIO as GPIO
 
-pin = 5 # GPIO pin
+pin = 3 # GPIO pin
 debug = True
+uri = "http://%piholeIP%/admin/api.php?disable=%secondstodisable%&auth=%yourapikey%"
 
 
 def button_callback(channel):
     if (debug):
         print("Button was pushed!")
+    requests.get(uri)
 
-GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(pin,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+GPIO.add_event_detect(pin, GPIO.RISING, callback=button_callback) # Setup event on pin 10 rising edge
 
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
     GPIO.cleanup() # resets all GPIO ports used by this program
