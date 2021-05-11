bdb41d3ba5af0eb0f35a7f16b6f736ee5ab0d8dd
Joel <joel.hamilton@fusionnetworks.co.nz>
2021-05-12 10:20:13 +1200

    tidy up python

--- scripts/pushButton.py
+++ scripts/pushButton.py
@@ -1,14 +1,21 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
 
-pin = 5
+pin = 5 # GPIO pin
+debug = True
+
+
 def button_callback(channel):
-    print("Button was pushed!")
+    if (debug):
+        print("Button was pushed!")
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) 
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 GPIO.add_event_detect(pin,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+
+except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
+    GPIO.cleanup() # resets all GPIO ports used by this program
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
--- scripts/run-fan.py
+++ scripts/run-fan.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
+
 from time import sleep
 import os
-import signal
-import sys
 import RPi.GPIO as GPIO
-pin = 14 # The pin ID, edit here to change it
-maxTMP = 54 # The maximum temperature in Celsius after which we trigger the fandef setup():
-minTMP = 40
+
+pin = 14 # GPIO pin
+maxTMP = 55 # The temperature in Celsius after which we trigger the fan
+minTMP = 40 # The temperature in Celsius after which we stop the fan
 sleepTime = 5
+debug = False
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 def getCPUtemperature():
     res = os.popen("vcgencmd measure_temp").readline()
     temp =(res.replace("temp=","").replace("'C\n",""))
-#    print("temp is {0}".format(temp)) #Uncomment here for testing
+    if (debug):
+        print("temp is {0}".format(temp)) #Uncomment here for testing
+
     return temp
+
 def fanON():
     setPin(True)
     return()
+
 def fanOFF():
     setPin(False)
     return()
+
 def getTEMP():
     CPU_temp = float(getCPUtemperature())
     if CPU_temp>maxTMP:
@@ -29,9 +36,11 @@ def getTEMP():
     elif CPU_temp<minTMP:
         fanOFF()
     return()
-def setPin(mode): # A little redundant function but useful if you want to add logging
+
+def setPin(mode):
     GPIO.output(pin, mode)
     return()
+
 try:
     while True:
         getTEMP()
